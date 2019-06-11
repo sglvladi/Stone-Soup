7a618f8a97573fdf546f7f43dce3f9fa838548a5
JonathanWillitts <jonathan.willitts@liverpool.ac.uk>
2019-06-11 15:56:23 +0100

    Disable drop connection flag
    (this needs to be done earlier so as to only drop the collection when
    the script is initially run

--- examples/track_analytics/AisTrackingTest.py
+++ examples/track_analytics/AisTrackingTest.py
@@ -230,7 +230,7 @@ for i, filename in enumerate(filenames):
                      host_port=27017,
                      db_name="TA_IHS",
                      collection_name=["Live_SS_Tracks", "Live_SS_Points"],
-                     drop=True)
+                     drop=False)
 
         # Plot the data
         # plt.clf()
