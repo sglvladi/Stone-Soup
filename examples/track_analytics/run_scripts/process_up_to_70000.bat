513a718ca74c8e0fac44642745630d1cb6515b2a
JonathanWillitts <jonathan.willitts@liverpool.ac.uk>
2019-09-10 08:56:16 +0100

    Add (split workload) run scripts

---
+++ examples/track_analytics/run_scripts/process_up_to_70000.bat
@@ -0,0 +1,16 @@
+REM Activate python virtual environment
+CALL ..\..\..\venv\Scripts\activate.bat
+
+REM Define group/block
+SET mmsi_group=70000
+
+REM Define MMSI data root folder
+SET data_root=C:\mmsi_files
+
+REM update Python Path to allow import of top-level (stonesoup) packages
+SET PYTHONPATH=%PYTHONPATH%;..\..\..\;
+
+REM Process through SS
+ECHO Processing group block up to: %mmsi_group%...
+python ..\AisTrackingExactEarthIMM_Single_Bare.py --input_path %data_root%\up_to_%mmsi_group% --output_path %data_root%\output_to_%mmsi_group%
+pause
