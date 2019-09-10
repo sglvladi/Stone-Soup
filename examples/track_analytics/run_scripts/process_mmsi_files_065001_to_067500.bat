00e10b43a3ec6f60c86f1ddf73ee2c1ba0a80522
JonathanWillitts <Jonathan.Willitts@liverpool.ac.uk>
2019-09-10 15:47:02 +0100

    Add (generated) run files, delete old ones

---
+++ examples/track_analytics/run_scripts/process_mmsi_files_065001_to_067500.bat
@@ -0,0 +1,17 @@
+
+REM Activate python virtual environment
+CALL ..\..\..\venv\Scripts\activate.bat
+
+REM Define group/block
+SET mmsi_group=mmsi_files_065001_to_067500
+
+REM Define MMSI data root folder
+SET data_root=C:\mmsi_files
+
+REM update Python Path to allow import of top-level (stonesoup) packages
+SET PYTHONPATH=%PYTHONPATH%;..\..\..\;
+
+REM Process through SS
+ECHO Processing: %mmsi_group%...
+python ..\AisTrackingExactEarthIMM_Single_Bare.py --input_path %data_root%\%mmsi_group% --output_path %data_root%\output_for_%mmsi_group%
+pause
--- examples/track_analytics/run_scripts/process_mmsi_files_082501_to_085000.bat
+++ examples/track_analytics/run_scripts/process_mmsi_files_082501_to_085000.bat
@@ -1,8 +1,9 @@
+
 REM Activate python virtual environment
 CALL ..\..\..\venv\Scripts\activate.bat
 
 REM Define group/block
-SET mmsi_group=40000
+SET mmsi_group=mmsi_files_082501_to_085000
 
 REM Define MMSI data root folder
 SET data_root=C:\mmsi_files
@@ -11,6 +12,6 @@ REM update Python Path to allow import of top-level (stonesoup) packages
 SET PYTHONPATH=%PYTHONPATH%;..\..\..\;
 
 REM Process through SS
-ECHO Processing group block up to: %mmsi_group%...
-python ..\AisTrackingExactEarthIMM_Single_Bare.py --input_path %data_root%\up_to_%mmsi_group% --output_path %data_root%\output_to_%mmsi_group%
+ECHO Processing: %mmsi_group%...
+python ..\AisTrackingExactEarthIMM_Single_Bare.py --input_path %data_root%\%mmsi_group% --output_path %data_root%\output_for_%mmsi_group%
 pause
