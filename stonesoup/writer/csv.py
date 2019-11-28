853956e383d32795647c68e0c0b1cc48ee662dac
sglvladi <sglvladi@gmail.com>
2019-11-28 19:14:55 +0000

    Added CSV writer

--- stonesoup/writer/csv.py
+++ stonesoup/writer/csv.py
@@ -10,9 +10,9 @@ from ..base import Property
 from ..functions import mod_bearing
 from ..types.prediction import StatePrediction
 
-class CSV_Writer_EE_MMSI(Writer):
+class CSV_Writer_NZ(Writer):
     """
-    This is a CSV Writer for the Exact Earth data, specifically configured
+    This is a CSV Writer for the NZ data, specifically configured
     to work with the "per mmsi" approach. It is expected that a new writer
     will be instantiated for each mmsi file.
 
@@ -20,74 +20,28 @@ class CSV_Writer_EE_MMSI(Writer):
     ----------
     save_dir: str
         The directory where the output files should be saved
-    mmsi: str
+    filename: str
         The name of the
     """
     save_dir = Property(str, doc="Save directory")
-    mmsi = Property(str, doc="MMSI")
+    filename = Property(str, doc="Filename")
 
     def __init__(self, save_dir, mmsi, *args, **kwargs):
         super().__init__(save_dir, mmsi, *args, **kwargs)
-        self.main_fields = ['ID', 'TrackID', 'Latitude', 'Longitude',
-                            'ReceivedTime', 'ReceivedTimeDate', 'DataType',
-                            'Speed', 'Heading', 'LRIMOShipNo', 'MMSI',
-                            'ShipName', 'ShipType', 'AdditionalInfo',
-                            'DetectionHistory', 'CallSign', 'Draught',
-                            'Destination', 'Location','StoppedProbability']
-        self.new_fields = ['Message_ID', 'Repeat_indicator', 'Time',
-                           'Millisecond', 'Region', 'Country', 'Base_station',
-                           'Online_data', 'Group_code', 'Sequence_ID',
-                           'Channel', 'Data_length', 'Call_sign', 'IMO',
-                           'Dimension_to_Bow', 'Dimension_to_stern',
-                           'Dimension_to_port', 'Dimension_to_starboard',
-                           'AIS_version', 'Navigational_status', 'ROT',
-                           'Accuracy', 'RawLongitude', 'RawLatitude', 'COG',
-                           'Regional', 'Maneuver', 'RAIM_flag',
-                           'Communication_flag', 'Communication_state',
-                           'UTC_year', 'UTC_month', 'UTC_day', 'UTC_hour',
-                           'UTC_minute', 'UTC_second', 'Fixing_device',
-                           'Transmission_control', 'ETA_month', 'ETA_day',
-                           'ETA_hour', 'ETA_minute', 'Sequence',
-                           'Destination_ID', 'Retransmit_flag', 'Country_code',
-                           'Functional_ID', 'Data', 'Destination_ID_1',
-                           'Sequence_1', 'Destination_ID_2', 'Sequence_2',
-                           'Destination_ID_3', 'Sequence_3',
-                           'Destination_ID_4', 'Sequence_4', 'Altitude',
-                           'Altitude_sensor', 'Data_terminal', 'Mode',
-                           'Safety_text', 'Non-standard_bits',
-                           'Name_extension', 'Name_extension_padding',
-                           'Message_ID_1_1', 'Offset_1_1', 'Message_ID_1_2',
-                           'Offset_1_2', 'Message_ID_2_1', 'Offset_2_1',
-                           'Destination_ID_A', 'Offset_A', 'Increment_A',
-                           'Destination_ID_B', 'offsetB', 'incrementB',
-                           'data_msg_type', 'station_ID', 'Z_count',
-                           'num_data_words', 'health', 'unit_flag',
-                           'display', 'DSC', 'band', 'msg22', 'offset1',
-                           'num_slots1', 'timeout1', 'Increment_1', 'Offset_2',
-                           'Number_slots_2', 'Timeout_2', 'Increment_2',
-                           'Offset_3', 'Number_slots_3', 'Timeout_3',
-                           'Increment_3', 'Offset_4', 'Number_slots_4',
-                           'Timeout_4', 'Increment_4', 'ATON_type',
-                           'ATON_name', 'off_position', 'ATON_status',
-                           'Virtual_ATON', 'Channel_A', 'Channel_B',
-                           'Tx_Rx_mode', 'Power', 'Message_indicator',
-                           'Channel_A_bandwidth', 'Channel_B_bandwidth',
-                           'Transzone_size', 'Longitude_1', 'Latitude_1',
-                           'Longitude_2', 'Latitude_2', 'Station_Type',
-                           'Report_Interval', 'Quiet_Time', 'Part_Number',
-                           'Vendor_ID', 'Mother_ship_MMSI',
-                           'Destination_indicator', 'Binary_flag',
-                           'GNSS_status', 'spare', 'spare2', 'spare3',
-                           'spare4', 'InvalidLonLat', 'ZeroZeroLonLat',
-                           'ModifiedLonLat', 'type']
-        self.fields = self.main_fields + self.new_fields
+        self.fields = ['ID', 'TrackID', 'Latitude', 'Longitude',
+                       'ReceivedTime', 'DataType', 'Speed', 'Heading',
+                       'LRIMOShipNo', 'MMSI', 'ShipName', 'ShipType',
+                       'AdditionalInfo', 'DetectionHistory', 'CallSign',
+                       'Draught', 'Destination', 'Location',
+                       'StoppedProbability', 'LatitudeDot', 'LongitudeDot']
+
         with open(os.path.join(self.save_dir,'tracks_{}.csv'.format(
-                self.mmsi)),
+                self.filename)),
                   'a', newline='') as f:
             writer = csv.DictWriter(f, fieldnames=self.fields)
             writer.writeheader()
         with open(os.path.join(self.save_dir,'points_{}.csv'.format(
-                self.mmsi)),
+                self.filename)),
                   'a', newline='') as f:
             writer = csv.DictWriter(f, fieldnames=self.fields)
             writer.writeheader()
@@ -118,11 +72,11 @@ class CSV_Writer_EE_MMSI(Writer):
                 'Longitude': float(track.last_update.state_vector[0, 0]),
                 'Latitude': float(track.last_update.state_vector[2, 0])}
             stopped_prob = track.state.weights[1, 0]
-            speed = float(metadata['SOG']) \
-                if (metadata['SOG'] and not metadata['SOG'] == 'None')\
+            speed = float(metadata['SOG (knts)']) \
+                if (metadata['SOG (knts)'] and not metadata['SOG (knts)'] == 'None')\
                 else 0
-            heading = float(metadata['Heading']) \
-                if (metadata['Heading'] and not metadata['Heading'] == 'None') \
+            heading = float(metadata['COG (deg)']) \
+                if (metadata['COG (deg)'] and not metadata['COG (deg)'] == 'None') \
                 else 0
             heading = mod_bearing(-np.deg2rad(heading) + np.pi/2)
 
@@ -130,7 +84,7 @@ class CSV_Writer_EE_MMSI(Writer):
             for state in track.states:
                 if hasattr(state, 'hypothesis'):
                     detection_history.append(
-                        state.hypothesis.measurement.metadata.get('ID'))
+                        state.hypothesis.measurement.id)
 
             # Standardise date/timestamps into both epoch, and date formats
             received_date = datetime.fromtimestamp(
@@ -142,7 +96,9 @@ class CSV_Writer_EE_MMSI(Writer):
                 'ID': track.id,  # TODO: confirm required as well as TrackID
                 'TrackID': track.id,
                 'Latitude': latest_position.get('Latitude'),
+                'LatitudeDot': float(track.last_update[1, 0]),
                 'Longitude': latest_position.get('Longitude'),
+                'LongitudeDot': float(track.last_update[3, 0]),
                 'ReceivedTime': received_epoch_in_ms,
                 'ReceivedTimeDate': received_date,
                 'DataType': 'fused',
@@ -170,7 +126,7 @@ class CSV_Writer_EE_MMSI(Writer):
             track_documents.append(copy(doc))
         # tracks_collection.insert_many(track_documents)
         with open(os.path.join(self.save_dir,'tracks_{}.csv'.format(
-                self.mmsi)),
+                self.filename)),
                   'a', newline='') as f:
             writer = csv.DictWriter(f, fieldnames=self.fields)
             writer.writerows(track_documents)
@@ -182,11 +138,11 @@ class CSV_Writer_EE_MMSI(Writer):
                 'Longitude': float(detection.state_vector[0, 0]),
                 'Latitude': float(detection.state_vector[1, 0]),
             }
-            speed = float(metadata['SOG']) \
-                if (metadata['SOG'] and not metadata['SOG'] == 'None') \
+            speed = float(metadata['SOG (knts)']) \
+                if (metadata['SOG (knts)'] and not metadata['SOG (knts)'] == 'None') \
                 else 0
-            heading = float(metadata['Heading']) \
-                if (metadata['Heading'] and not metadata['Heading'] == 'None') \
+            heading = float(metadata['COG (deg)']) \
+                if (metadata['COG (deg)'] and not metadata['COG (deg)'] == 'None') \
                 else 0
             heading = mod_bearing(-np.deg2rad(heading) + np.pi / 2)
 
@@ -218,15 +174,12 @@ class CSV_Writer_EE_MMSI(Writer):
                     'coordinates': position
                 }
             }
-            for field in self.new_fields:
-                dicts = {field: detection.metadata.get(field)}
-                doc.update(dicts)
 
             # Insert values into Mongo
             point_documents.append(doc)
 
         with open(os.path.join(self.save_dir,'points_{}.csv'.format(
-                self.mmsi)),
+                self.filename)),
                   'a', newline='') as f:
             writer = csv.DictWriter(f, fieldnames=self.fields)
             writer.writerows(point_documents)
