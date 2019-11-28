3487668768089c30260a2e7bb0a64b8c39972a8e
sglvladi <sglvladi@gmail.com>
2019-11-28 01:38:45 +0000

    Add pymongo and kafka-python to setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,8 @@ setup(name='stonesoup',
       ],
       packages=find_packages(exclude=('docs', '*.tests')),
       install_requires=[
-          'ruamel.yaml>=0.15.45', 'numpy>=1.17', 'scipy', 'matplotlib', 'utm', 'pymap3d'],
+          'ruamel.yaml>=0.15.45', 'numpy>=1.17', 'scipy', 'matplotlib', 'utm', 'pymap3d',
+          'pymongo', 'kafka-python'],
       extras_require={
           'dev': [
               'pytest-flake8', 'pytest-cov', 'Sphinx', 'sphinx_rtd_theme',
