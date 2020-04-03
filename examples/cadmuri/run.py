d83a88fa4e692b715752f3de7bb33b428a0e00f5
sglvladi <sglvladi@gmail.com>
2020-04-03 22:26:38 +0100

    Example update

--- examples/cadmuri/run.py
+++ examples/cadmuri/run.py
@@ -10,17 +10,17 @@ from stonesoup.custom.plotting import plot_network
 from stonesoup.custom.simulation import simulate
 from stonesoup.models.transition.destination import DestinationTransitionModel
 from stonesoup.models.measurement.destination import DestinationMeasurementModel
-from stonesoup.predictor.particle import ParticlePredictor
-from stonesoup.resampler.particle import SystematicResampler
+from stonesoup.predictor.particle import ParticlePredictor, ParticlePredictor2
+from stonesoup.resampler.particle import SystematicResampler, SystematicResampler2
 from stonesoup.types.array import StateVector
 from stonesoup.types.hypothesis import SingleHypothesis
-from stonesoup.types.state import ParticleState
+from stonesoup.types.state import ParticleState, ParticleState2
 from stonesoup.types.detection import Detection
 from stonesoup.types.particle import Particle
 
 # Load Graph
 from stonesoup.types.track import Track
-from stonesoup.updater.particle import ParticleUpdater
+from stonesoup.updater.particle import ParticleUpdater, ParticleUpdater2
 
 path = r'C:\Users\sglvladi\OneDrive\Workspace\PostDoc\CADMURI\MATLAB\simple\data\minn_2.mat'
 S = load_graph_dict(path)
@@ -29,11 +29,13 @@ num_nodes = G.number_of_nodes()
 num_edges = G.number_of_edges()
 
 # Global vars
-num_destinations = 10
-num_particles =500
+USE_NEW_PF = True
+num_destinations = 100
+num_particles = 1000
 source = 519
 destination = 115
 speed = 0.1
+
 
 # Simulate ground-truth
 gnd_path, gnd_route_n, gnd_route_e = simulate(G, source, destination, speed)
@@ -49,12 +51,13 @@ for i in range(num_destinations):
     short_paths_n[dest], short_paths_e[dest] = shortest_path(G, source, dest)
 
 # Transition model
-transition_model = DestinationTransitionModel(0.001, G, short_paths_e)
+transition_model = DestinationTransitionModel(0.001, S, short_paths_e)
 
 # Measurement model
 mapping = [0,1]
 R = np.eye(2)*0.0002
-measurement_model = DestinationMeasurementModel(ndim_state=4, mapping=mapping, noise_covar=R, spaths=short_paths_e, graph=S)
+measurement_model = DestinationMeasurementModel(ndim_state=4, mapping=mapping, noise_covar=R,
+                                                spaths=short_paths_e, graph=S)
 
 # Simulate detections
 scans = []
@@ -73,36 +76,52 @@ prior_speed = mvn.rvs(0, speed, (num_particles,))
 prior_e = np.ones((num_particles,))*gnd_route_e[0]
 prior_destinations = np.random.choice(destinations, (num_particles,))
 prior_particles = []
-for r, s, e, d in zip(prior_r, prior_speed, prior_e, prior_destinations):
-    particle = Particle(StateVector([r, s, e, d]), 1.0/num_particles)
+prior_particle_sv = np.zeros((4,num_particles))
+for i, sv in enumerate(zip(prior_r, prior_speed, prior_e, prior_destinations)):
+    particle = Particle(StateVector(list(sv)), 1.0/num_particles)
     prior_particles.append(particle)
+    prior_particle_sv[:,i] = np.array(sv)
 prior_state = ParticleState(particles=prior_particles, timestamp=timestamp_init)
+prior_state2 = ParticleState2(particles=prior_particle_sv, timestamp=timestamp_init)
 
-# Predictor
-predictor = ParticlePredictor(transition_model)
+if USE_NEW_PF:
+    # Predictor
+    predictor = ParticlePredictor2(transition_model)
+    # Updater
+    resampler = SystematicResampler2()
+    updater = ParticleUpdater2(measurement_model, resampler)
+    # Initiate track
+    track = Track(prior_state2)
+else:
+    # Predictor
+    predictor = ParticlePredictor(transition_model)
+    # Updater
+    resampler = SystematicResampler()
+    updater = ParticleUpdater(measurement_model, resampler)
+    # Initiate track
+    track = Track(prior_state)
 
-# Updater
-resampler = SystematicResampler()
-updater = ParticleUpdater(measurement_model, resampler)
 
 fig, ax = plt.subplots()
-track = Track(prior_state)
 pr = profile.Profile()
 pr.disable()
 for timestamp, detections in scans:
     print(timestamp)
-    pr.enable()
-    prediction = predictor.predict(track.state, timestamp=timestamp)
-    pr.disable()
     detection = list(detections)[0]
-    hypothesis = SingleHypothesis(prediction, detection)
     pr.enable()
+    # Run PF
+    prediction = predictor.predict(track.state, timestamp=timestamp)
+    hypothesis = SingleHypothesis(prediction, detection)
     posterior = updater.update(hypothesis)
-    pr.disable()
     track.append(posterior)
 
+    pr.disable()
+
     # Compute statistics
-    data = np.array([p.state_vector for p in posterior.particles])[:, :, 0].T
+    if USE_NEW_PF:
+        data = posterior.particles
+    else:
+        data = np.array([p.state_vector for p in posterior.particles])[:, :, 0].T
 
     # Compute counts of destinations and current positions
     v_dest, vd_counts = np.unique(data[3,:], return_counts=True)
@@ -118,4 +137,5 @@ for timestamp, detections in scans:
     plt.plot(xy[0,:],xy[1,:],'.r')
     plt.pause(0.0001)
 
-pr.dump_stats('profile.pstat')
+pf = 'new' if USE_NEW_PF else 'old'
+pr.dump_stats('profile_{}.pstat'.format(pf))
