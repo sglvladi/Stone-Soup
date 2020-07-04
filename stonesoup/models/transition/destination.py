96979e97a51f861a2b1a3cc899d327a01120e406
sglvladi <sglvladi@gmail.com>
2020-07-04 01:09:02 +0100

    Model updates

--- stonesoup/models/transition/destination.py
+++ stonesoup/models/transition/destination.py
@@ -8,7 +8,8 @@ from ...base import Property
 from ..base import LinearModel, NonLinearModel
 from .base import TransitionModel
 from .linear import ConstantVelocity, LinearGaussianTransitionModel
-from ...custom.graph import CustomDiGraph
+from ...custom.graph import CustomDiGraph, normalise_re
+
 
 class DestinationTransitionModel(LinearGaussianTransitionModel):
     noise_diff_coeff = Property(
@@ -19,18 +20,18 @@ class DestinationTransitionModel(LinearGaussianTransitionModel):
 
     @property
     def ndim_state(self):
-        return 4
+        return 5
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.cv_model = ConstantVelocity(self.noise_diff_coeff)
 
     def matrix(self, time_interval, **kwargs):
-        transition_matrices = [self.cv_model.matrix(time_interval, **kwargs), np.eye(2)]
+        transition_matrices = [self.cv_model.matrix(time_interval, **kwargs), np.eye(3)]
         return block_diag(*transition_matrices)
 
     def covar(self, time_interval, **kwargs):
-        covar_list = [self.cv_model.covar(time_interval, **kwargs), np.zeros((2,2))]
+        covar_list = [self.cv_model.covar(time_interval, **kwargs), np.zeros((3,3))]
         return block_diag(*covar_list)
 
     def function(self, state, time_interval, noise=None, **kwargs):
@@ -43,56 +44,72 @@ class DestinationTransitionModel(LinearGaussianTransitionModel):
         if noise is None:
             noise = self.rvs(num_particles, time_interval=time_interval, **kwargs)
 
-        # CV (Position-Velocity) Propagation
+        # 1) CV (Position-Velocity) Propagation
         if num_particles > 1:
             n_state_vectors = self.matrix(time_interval) @ state.particles + noise
         else:
             n_state_vectors = self.matrix(time_interval)@state.state_vector + noise
 
-        r = n_state_vectors[0, :]
-        e = n_state_vectors[2, :]
-        d = n_state_vectors[3, :]
-
+        # 2) SMC destination sampling
         # Get all valid destinations given the current edge
-        v_dest = []
+        v_dest = dict()
+        e = n_state_vectors[2, :]
         u_edges = np.unique(e)
         for edge in u_edges:
             for dest, path in self.spaths.items():
                 # If the path contains the edge
                 if len(np.where(path == edge)[0]) > 0:
-                    v_dest.append(dest)
+                    if edge in v_dest:
+                        v_dest[edge].append(dest[1])
+                    else:
+                        v_dest[edge] = [dest[1]]
+        # Perform the sampling
+        for i in range(num_particles):
+            if np.random.rand() > 0.98:
+                edge = e[i]
+                n_state_vectors[3, i] = np.random.choice(v_dest[edge])
+
+        # 3) Process edge change
+        # The CV propagation may lead to r's which are either less that zero or more than the
+        # length of the edge. This means that the range and edge identifier needs to be adjusted
+        # to correctly place the particle.
+
+        # Get shortcuts for faster accessing
+        r = n_state_vectors[0, :]
+        e = n_state_vectors[2, :]
+        d = n_state_vectors[3, :]
+        s = n_state_vectors[4, :]
 
         for i in range(num_particles):
-            r_i = r[i]
-            e_i = e[i]
-            d_i = d[i]
-            edge_len = self.graph['Edges']['Weight'][int(e_i)]
-            path = self.spaths[d_i]
-            idx = np.where(path == e_i)[0]  # Find path index of current edge
-            if len(idx) > 0 and len(path) > idx[0] + 1:
-                idx = idx[0]
-                # If particle has NOT reached the end of the path
-                while r_i > edge_len:
-                    r_i = r_i - edge_len
-                    e_i = path[idx + 1]
-                    edge_len = self.graph['Edges']['Weight'][int(e_i)]
-                    idx = idx + 1
-                    if len(path) == idx + 1:
-                        # If particle has reached the end of the path
-                        if r_i > edge_len:
-                            # Cap r_i to edge_length
-                            r_i = edge_len
-                        break
-            elif len(idx) > 0 and len(path) == idx[0] + 1:
-                # If particle has reached the end of the path
-                if r_i > edge_len:
-                    # Cap r_i to edge_length
-                    r_i = edge_len
+            r_i, e_i, d_i, s_i = normalise_re(r[i], e[i], d[i], s[i], self.spaths, self.graph)
+            # r_i = r[i]
+            # e_i = e[i]
+            # d_i = d[i]
+            # s_i = s[i]
+            # edge_len = self.graph['Edges']['Weight'][int(e_i)]
+            # path = self.spaths[(s_i, d_i)]
+            # idx = np.where(path == e_i)[0]  # Find path index of current edge
+            # if len(idx) > 0 and len(path) > idx[0] + 1:
+            #     idx = idx[0]
+            #     # If particle has NOT reached the end of the path
+            #     while r_i > edge_len:
+            #         r_i = r_i - edge_len
+            #         e_i = path[idx + 1]
+            #         edge_len = self.graph['Edges']['Weight'][int(e_i)]
+            #         idx = idx + 1
+            #         if len(path) == idx + 1:
+            #             # If particle has reached the end of the path
+            #             if r_i > edge_len:
+            #                 # Cap r_i to edge_length
+            #                 r_i = edge_len
+            #             break
+            # elif len(idx) > 0 and len(path) == idx[0] + 1:
+            #     # If particle has reached the end of the path
+            #     if r_i > edge_len:
+            #         # Cap r_i to edge_length
+            #         r_i = edge_len
 
             n_state_vectors[0, i] = r_i
             n_state_vectors[2, i] = e_i
-
-            if np.random.rand() > 0.98:
-                n_state_vectors[3, i] = np.random.choice(v_dest)
 
         return n_state_vectors
