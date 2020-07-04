7464e22a25ea79ff9cfc32569416ac7efffbaab5
sglvladi <sglvladi@gmail.com>
2020-07-04 01:18:01 +0100

    Custom package updates

--- stonesoup/custom/plotting.py
+++ stonesoup/custom/plotting.py
@@ -1,5 +1,38 @@
 import networkx as nx
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+from stonesoup.base import Base, Property
+
+# class CustomPlotter(Base):
+#     meta = Property(dict, default=None)
+#
+#     def __init__(self, *args, **kwargs):
+#         if self.meta is None:
+
+
+
+
+def add_subplot_axes(ax, rect, axisbg='w'):
+    fig = plt.gcf()
+    box = ax.get_position()
+    width = box.width
+    height = box.height
+    inax_position  = ax.transAxes.transform(rect[0:2])
+    transFigure = fig.transFigure.inverted()
+    infig_position = transFigure.transform(inax_position)
+    x = infig_position[0]
+    y = infig_position[1]
+    width *= rect[2]
+    height *= rect[3]  # <= Typo was here
+    subax = fig.add_axes([x,y,width,height],axisbg=axisbg)
+    x_labelsize = subax.get_xticklabels()[0].get_size()
+    y_labelsize = subax.get_yticklabels()[0].get_size()
+    x_labelsize *= rect[2]**0.5
+    y_labelsize *= rect[3]**0.5
+    subax.xaxis.set_tick_params(labelsize=x_labelsize)
+    subax.yaxis.set_tick_params(labelsize=y_labelsize)
+    return subax
 
 
 def plot_network(G, ax, node_size=0.1, width=0.5, with_labels=False):
@@ -7,7 +40,60 @@ def plot_network(G, ax, node_size=0.1, width=0.5, with_labels=False):
     pos = nx.get_node_attributes(G, 'pos')
 
     nx.draw_networkx(G, pos, arrows=False, ax=ax, node_size=node_size, width=width, with_labels=with_labels)
-    plt.xlabel('Longitude')
-    plt.ylabel('Latitude')
-    plt.axis('on')
-    ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
+    # plt.xlabel('Longitude')
+    # plt.ylabel('Latitude')
+    # plt.axis('on')
+    # ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
+
+
+def highlight_nodes(G, ax,  nodes, node_size=0.1, node_color='m', node_shape='s', label=None):
+    # Get node positions
+    pos = nx.get_node_attributes(G, 'pos')
+
+    nx.draw_networkx_nodes(G, pos, nodelist=nodes, ax=ax, node_size=node_size,
+                           node_color=node_color, node_shape=node_shape, label=label)
+
+def highlight_edges(G, ax, edges_idx, width= 2.0, edge_color='m', style='solid', arrows=False, label=None):
+    edges = G.edges_by_idx(edges_idx)
+    pos = nx.get_node_attributes(G, 'pos')
+
+    nx.draw_networkx_edges(G, pos, edgelist=edges, ax=ax, width=width, edge_color=edge_color,
+                           style=style, arrows=arrows, label=label)
+
+def plot_cov_ellipse(cov, pos, nstd=2, ax=None, **kwargs):
+    """
+    Plots an `nstd` sigma error ellipse based on the specified covariance
+    matrix (`cov`). Additional keyword arguments are passed on to the
+    ellipse patch artist.
+    Parameters
+    ----------
+        cov : The 2x2 covariance matrix to base the ellipse on
+        pos : The location of the center of the ellipse. Expects a 2-element
+            sequence of [x0, y0].
+        nstd : The radius of the ellipse in numbers of standard deviations.
+            Defaults to 2 standard deviations.
+        ax : The axis that the ellipse will be plotted on. Defaults to the
+            current axis.
+        Additional keyword arguments are pass on to the ellipse patch.
+    Returns
+    -------
+        A matplotlib ellipse artist
+    """
+
+    def eigsorted(cov):
+        vals, vecs = np.linalg.eigh(cov)
+        order = vals.argsort()[::-1]
+        return vals[order], vecs[:, order]
+
+    if ax is None:
+        ax = plt.gca()
+
+    vals, vecs = eigsorted(cov)
+    theta = np.degrees(np.arctan2(*vecs[:, 0][::-1]))
+
+    # Width and height are "full" widths, not radius
+    width, height = 2 * nstd * np.sqrt(vals)
+    ellip = Ellipse(xy=pos, width=width, height=height, angle=theta, **kwargs)
+
+    ax.add_artist(ellip)
+    return ellip
--- stonesoup/custom/simulation.py
+++ stonesoup/custom/simulation.py
@@ -5,11 +5,13 @@ from datetime import datetime,timedelta
 from stonesoup.custom.graph import shortest_path
 from stonesoup.functions import pol2cart, cart2pol
 from stonesoup.types.array import StateVector
+from stonesoup.types.detection import Detection
 from stonesoup.types.groundtruth import GroundTruthPath
 from stonesoup.types.groundtruth import GroundTruthState
 
 
-def simulate(G: nx.DiGraph, source: int, destination: int, speed: float, interval: timedelta = timedelta(seconds=1)):
+def simulate(G: nx.DiGraph, source: int, destination: int, speed: float, track_id: int = None,
+             timestamp_init: datetime = datetime.now(), interval: timedelta = timedelta(seconds=1)):
     """ Simulate a moving target along the network
 
     Parameters
@@ -22,6 +24,8 @@ def simulate(G: nx.DiGraph, source: int, destination: int, speed: float, interva
         The destination node index
     speed: :class:`float`
         The speed of the target (assumed to be constant)
+    timestamp_init: :class:`datetime.datetime`
+        The initial timestamp
     interval: :class:`datetime.timedelta`
         The interval between ground-truth reports
 
@@ -44,9 +48,9 @@ def simulate(G: nx.DiGraph, source: int, destination: int, speed: float, interva
 
     # Initialize the Ground-Truth path
     sv = StateVector(np.array(pos[gnd_route_n[0]]))
-    timestamp = datetime.now()
+    timestamp = timestamp_init
     state = GroundTruthState(sv, timestamp=timestamp)
-    gnd_path = GroundTruthPath([state])
+    gnd_path = GroundTruthPath([state], id = track_id)
 
     r = 0  # Stores the distance travelled (range) along a given edge
     overflow = False  # Indicates when the range has overflown to a new edge
@@ -104,3 +108,110 @@ def simulate(G: nx.DiGraph, source: int, destination: int, speed: float, interva
                     gnd_path.append(state)
 
     return gnd_path, gnd_route_n, gnd_route_e
+
+
+def simulate_gnd(G, num_tracks, num_destinations, speed):
+    t_sources = []
+    t_destinations = []
+    t_colors = []
+    num_nodes = G.number_of_nodes()
+    num_edges = G.number_of_edges()
+
+    for i in range(num_tracks):
+        t_len = 0
+        print(i)
+        while t_len < 20 or t_len > 100:
+            t_source = int(np.random.uniform(0, num_nodes))
+            t_dest = int(np.random.uniform(0, num_nodes))
+            path_n, path_e = shortest_path(G, t_source, t_dest)
+            t_len = len(path_e)
+        t_sources.append(t_source)
+        t_destinations.append(t_dest)
+        t_colors.append(np.random.rand(3, ))
+
+    # Simulate ground-truth
+    gnd_paths = set()
+    gnd_routes_n = dict()
+    gnd_routes_e = dict()
+    timestamp_init = datetime.now()
+    for i in range(num_tracks):
+        gnd_path, gnd_route_n, gnd_route_e = simulate(G, t_sources[i], t_destinations[i], speed,
+                                                      timestamp_init=timestamp_init, track_id=i)
+        gnd_paths.add(gnd_path)
+        gnd_routes_e[gnd_path] = gnd_route_e
+        gnd_routes_n[gnd_path] = gnd_route_n
+
+    feed = t_destinations
+    feed_tmp = set([i for i in range(num_nodes)]) - set(feed)
+    destinations = feed + list(
+        np.random.choice(list(feed_tmp), (num_destinations - len(feed),), False))
+
+
+    return gnd_paths, gnd_routes_n, gnd_routes_e, t_sources, t_destinations, t_colors, destinations
+
+def simulate_detections(gnd_paths, measurement_model, P_D, lambda_FA, VBOUNDS):
+    # Simulate detections
+    scans = []
+    scans_dict = dict()
+    for gnd_path in gnd_paths:
+        for i, gnd_state in enumerate(gnd_path):
+            gnd_sv = gnd_state.state_vector
+            det_sv = gnd_sv + measurement_model.rvs()
+            timestamp = gnd_state.timestamp
+            detection = Detection(state_vector=det_sv, timestamp=timestamp)
+            if i == 0 or np.random.rand() <= P_D:
+                if timestamp in scans_dict:
+                    scans_dict[timestamp] |= set([detection])
+                else:
+                    scans_dict[timestamp] = set([detection])
+
+    for timestamp in sorted(scans_dict.keys()):
+        detections = scans_dict[timestamp]
+        # Add clutter
+        num_clutter = np.random.poisson(lambda_FA)
+        x = np.random.uniform(VBOUNDS[0][0], VBOUNDS[0][1], num_clutter)
+        y = np.random.uniform(VBOUNDS[1][0], VBOUNDS[1][1], num_clutter)
+        for x_pos, y_pos in zip(x, y):
+            detection = Detection(state_vector=StateVector([x_pos, y_pos]), timestamp=timestamp)
+            detections.add(detection)
+
+        scans.append((timestamp, detections))
+
+    return scans
+
+def compute_short_paths(G, sources, destinations):
+
+    num_sources = len(sources)
+    num_destinations = len(destinations)
+
+    # Pre-compute short_paths
+    short_paths_n = dict()
+    short_paths_e = dict()
+    for source in sources:
+        for destination in destinations:
+            print("Computing path: ({},{})".format(source, destination))
+            short_paths_n[(source, destination)], short_paths_e[(source, destination)] = \
+                shortest_path(G, source, destination)
+
+    return short_paths_e, short_paths_n
+
+def compute_all_short_paths(G, sources):
+
+    # Pre-compute short_paths
+    short_paths_n = dict()
+    short_paths_e = dict()
+
+    edges = [e for e in G.edges]
+    edges_index = {edge: edges.index(edge) for edge in edges}
+
+    for source in sources:
+        # print("Computing paths for node {}".format(source))
+        paths = nx.shortest_path(G, source, weight='weight')
+        for destination, node_path in sorted(paths.items()):
+            print("Computing path: ({},{})".format(source, destination))
+            path_edges = zip(node_path, node_path[1:])
+            edge_path = [edges_index[edge] for edge in path_edges]
+            short_paths_n[(source, destination)], short_paths_e[(source, destination)] = \
+                (node_path, edge_path)
+
+    return short_paths_e, short_paths_n
