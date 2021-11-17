99667c759e9926ea77a3fd6362426b3fbe03f4fe
sglvladi <sglvladi@gmail.com>
2021-11-17 20:25:25 +0000

    Defined PlatformTargetDetectionSimulator

--- stonesoup/simulator/platform.py
+++ stonesoup/simulator/platform.py
@@ -30,3 +30,28 @@ class PlatformDetectionSimulator(DetectionSimulator):
                     truths_to_be_measured = truths.union(self.platforms) - {platform}
                     detections = sensor.measure(truths_to_be_measured)
                     yield time, detections
+
+
+class PlatformTargetDetectionSimulator(PlatformDetectionSimulator):
+    """A simple platform detection simulator.
+
+    Processes ground truth data and generates :class:`~.Detection` data
+    according to a list of platforms by calling each sensor in these platforms.
+
+    """
+    targets: Sequence[Platform] = Property(
+        doc='List of target platforms to be detected'
+    )
+
+    @BufferedGenerator.generator_method
+    def detections_gen(self):
+        for time, truths in self.groundtruth:
+            for platform in self.platforms:
+                platform.move(time)
+            for platform in self.targets:
+                platform.move(time)
+            for platform in self.platforms:
+                for sensor in platform.sensors:
+                    truths_to_be_measured = truths.union(self.targets)
+                    detections = sensor.measure(truths_to_be_measured)
+                    yield time, detections
