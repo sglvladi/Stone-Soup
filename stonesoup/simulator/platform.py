58998ab2820254fe089bc4e45bde47cb565a3e16
sglvladi <sglvladi@gmail.com>
2021-12-03 14:53:17 +0000

    Add clutter generation to PlatformTargetDetectionSimulator

--- stonesoup/simulator/platform.py
+++ stonesoup/simulator/platform.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from typing import Sequence
 
+import numpy as np
+
 from ..base import Property
 from ..reader import GroundTruthReader
 from .base import DetectionSimulator
 from ..buffered_generator import BufferedGenerator
 from ..platform import Platform
+from ..types.numeric import Probability
+from ..types.detection import Clutter
+from ..types.groundtruth import GroundTruthState
 
 
 class PlatformDetectionSimulator(DetectionSimulator):
@@ -42,6 +47,9 @@ class PlatformTargetDetectionSimulator(PlatformDetectionSimulator):
     targets: Sequence[Platform] = Property(
         doc='List of target platforms to be detected'
     )
+    meas_range: np.ndarray = Property(default=None)
+    detection_probability: Probability = Property(default=0.9)
+    clutter_rate: float = Property(default=2.0)
 
     @BufferedGenerator.generator_method
     def detections_gen(self):
@@ -52,6 +60,15 @@ class PlatformTargetDetectionSimulator(PlatformDetectionSimulator):
                 platform.move(time)
             for platform in self.platforms:
                 for sensor in platform.sensors:
+                    # True detections
                     truths_to_be_measured = truths.union(self.targets)
                     detections = sensor.measure(truths_to_be_measured)
+                    # Clutter
+                    gnd_clutter = []
+                    for _ in range(np.random.poisson(self.clutter_rate)):
+                        sv = np.random.rand(len(self.meas_range), 1) * np.diff(self.meas_range) + self.meas_range[:, :1]
+                        sv.resize((4,1), refcheck=False)
+                        sv = sv[[0, 2, 1, 3],:]
+                        gnd_clutter.append(GroundTruthState(sv, time))
+                    detections |= sensor.measure(gnd_clutter)
                     yield time, detections
