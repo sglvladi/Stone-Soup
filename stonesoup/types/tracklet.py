ddc4b1b2c8f07dce6f5859a24ffa0070178bc9e0
sglvladi <sglvladi@gmail.com>
2022-01-19 11:57:27 +0000

    Tracklet is now a subclass of Track

--- stonesoup/types/tracklet.py
+++ stonesoup/types/tracklet.py
@@ -2,11 +2,9 @@ import datetime
 import uuid
 from typing import List
 
-
 from ..base import Property
 from .base import Type
 from .track import Track
-from .state import State
 
 
 class SensorTracks(Type):
@@ -21,17 +19,8 @@ class SensorTracklets(SensorTracks):
     pass
 
 
-class Tracklet(Type):
-    id: str = Property(doc='The tracklet id', default=None)
-    priors: List[State] = Property(doc='The trackler priors', default=None)
-    posteriors: List[State] = Property(doc='The tracklet posteriors', default=None)
-    sensor_id: str = Property(doc='The sensor id', default=None)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.priors = self.priors if self.priors else []
-        self.posteriors = self.posteriors if self.posteriors else []
+class Tracklet(Track):
+    pass
 
 
 class SensorScan(Type):
