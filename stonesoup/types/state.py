9adc891363758c4f28d7f83aabed36339e89b1e6
sglvladi <sglvladi@gmail.com>
2022-01-31 23:44:07 +0000

    Added weight and tag to TwoStateGaussianState

--- stonesoup/types/state.py
+++ stonesoup/types/state.py
@@ -430,6 +430,8 @@ class TwoStateGaussianState(GaussianState):
     """ A Gaussian state object representing the distribution :math:`p(x_{k+T}, x_{k} | Y)` """
     start_time: datetime.datetime = Property(doc='Timestamp at t_k')
     end_time: datetime.datetime = Property(doc='Timestamp at t_{k+T}')
+    weight: Probability = Property(default=0, doc="Weight of the Gaussian State.")
+    tag: str = Property(default=None, doc="Unique tag of the Gaussian State.")
     # scan_id: int = Property(doc='The scan id')
 
     @property
