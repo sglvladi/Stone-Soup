e9137a62444329d4bdf34119a2cefb60302b132e
Alberto Acuto <alberto.acuto9@gmail.com>
2024-07-25 15:38:46 +0100

    updated the example to test the code

--- docs/examples/pf_comparison_with_proposals.py
+++ docs/examples/pf_comparison_with_proposals.py
@@ -13,7 +13,7 @@ from stonesoup.types.groundtruth import GroundTruthState, GroundTruthPath
 from stonesoup.types.state import GaussianState
 from stonesoup.types.array import StateVector
 from stonesoup.types.detection import Detection
-from stonesoup.proposal.simple import PriorAsProposal, KFasProposal
+from stonesoup.proposal.simple import PriorAsProposal, KFasProposal, LocalKFasProposal
 
 from stonesoup.models.measurement.nonlinear import CartesianToBearingRange
 from stonesoup.models.measurement.linear import LinearGaussian
@@ -32,8 +32,8 @@ num_steps = 100
 start_time = datetime.now().replace(microsecond=0)
 
 # instantiate the transition model
-transition_model = CombinedLinearGaussianTransitionModel([ConstantVelocity(5.5),
-                                                          ConstantVelocity(5.5)])
+transition_model = CombinedLinearGaussianTransitionModel([ConstantVelocity(1.5),
+                                                          ConstantVelocity(1.5)])
 
 transition_model2 = CombinedLinearGaussianTransitionModel([ConstantAcceleration(1.5),
                                                           ConstantAcceleration(1.5)])
@@ -49,16 +49,16 @@ for k in range(1, num_steps):
                                   time_interval=timedelta(seconds=1)),
         timestamp=start_time + timedelta(seconds=1*k)))
 
-measurement_model = CartesianToBearingRange(  # relative to the first sensor
-    ndim_state=4,
-    mapping=(0, 2),
-    noise_covar=np.diag([np.radians(1), 5]),
-    translation_offset=np.array([[0], [0]]))
-
-# measurement_model = LinearGaussian(
+# measurement_model = CartesianToBearingRange(  # relative to the first sensor
 #     ndim_state=4,
 #     mapping=(0, 2),
-#     noise_covar=np.diag([200, 200]))
+#     noise_covar=np.diag([np.radians(1), 5]),
+#     translation_offset=np.array([[0], [0]]))
+
+measurement_model = LinearGaussian(
+    ndim_state=4,
+    mapping=(0, 2),
+    noise_covar=np.diag([200, 200]))
 
 
 measurements = []
@@ -70,13 +70,14 @@ for i, state in enumerate(truth):  # loop over the ground truth detections
                                        measurement_model=measurement_model))
 
 
-from stonesoup.plotter import AnimatedPlotterly
+from stonesoup.plotter import AnimatedPlotterly, Plotterly, Plotter
 
 time_steps = [start_time + timedelta(seconds=2*i) for i in range(num_steps)]
 
-plotter = AnimatedPlotterly(timesteps=time_steps)
+#plotter = AnimatedPlotterly(timesteps=time_steps)
+plotter = Plotterly() # Plotter()
 plotter.plot_ground_truths(truth, [0, 2])
-plotter.plot_measurements(measurements, [0, 2], marker=dict(color='blue'),
+plotter.plot_measurements(measurements, [0, 2], #marker=dict(color='blue'),
                           measurements_label='Detections')
 
 predictor = ParticlePredictor(transition_model)
@@ -88,12 +89,18 @@ x = PriorAsProposal(transition_model, measurement_model)
 y = KFasProposal(UnscentedKalmanPredictor(transition_model),
                  UnscentedKalmanUpdater(measurement_model))
 
+zz = LocalKFasProposal(UnscentedKalmanPredictor(transition_model),
+                 UnscentedKalmanUpdater(measurement_model))
 
 predictor1 = ParticlePredictor(transition_model, proposal=x)
 updater1 = ParticleUpdater(resampler=resampler, measurement_model=measurement_model, proposal=x)
 
 predictor2 = ParticlePredictor(transition_model, proposal=y)
 updater2 = ParticleUpdater(resampler=resampler, measurement_model=measurement_model, proposal=y)
+
+predictor3 = ParticlePredictor(transition_model, proposal=zz)
+updater3 = ParticleUpdater(resampler=resampler, measurement_model=measurement_model, proposal=zz)
+
 
 # Load the Particle state priors
 from stonesoup.types.state import GaussianState, ParticleState, StateVector
@@ -128,6 +135,10 @@ particle_prior3 = ParticleState(state_vector=None,
                                           particle_list=particles,
                                           timestamp=time_steps[0])
 
+particle_prior4 = ParticleState(state_vector=None,
+                                          particle_list=particles,
+                                          timestamp=time_steps[0])
+
 # Load the tracking components
 from stonesoup.types.hypothesis import SingleHypothesis
 from stonesoup.types.track import Track
@@ -135,6 +146,7 @@ from stonesoup.types.track import Track
 track1 = Track(particle_prior1)
 track2 = Track(particle_prior2)
 track3 = Track(particle_prior3)
+track4 = Track(particle_prior4)
 
 for detection in measurements:
     prediction = predictor.predict(particle_prior1, timestamp=detection.timestamp)
@@ -158,11 +170,19 @@ for detection in measurements:
     track3.append(post)
     particle_prior3 = track3[-1]
 
+    prediction = predictor3.predict(particle_prior4, timestamp=detection.timestamp,
+                                    detection=detection)
+    hypothesis = SingleHypothesis(prediction, detection)
+    post = updater3.update(hypothesis)
+    track4.append(post)
+    particle_prior4 = track4[-1]
+
     # print(np.sum(post.weight), np.sum(np.exp(post.log_weight)), ' RW')
     #sys.exit()
 
 #sys.exit()
 plotter.plot_tracks(track1, mapping=[0,2], track_label='standard')
-plotter.plot_tracks(track2, mapping=[0,2], track_label='proposal prior', line= dict(color='blue'))
-plotter.plot_tracks(track3, mapping=[0,2], track_label='proposal kf', line= dict(color='green'))
-plotter.fig.show()
+plotter.plot_tracks(track2, mapping=[0,2], track_label='proposal prior')#, line= dict(color='blue'))
+plotter.plot_tracks(track3, mapping=[0,2], track_label='proposal kf') #, line= dict(color='green'))
+plotter.plot_tracks(track4, mapping=[0,2], track_label='proposal kf local') #, line= dict(color='green'))
+plotter.fig.show() #savefig('test_save.png')
